1b824fa07f3dedfcc70cee18c9966f4770e13678
pedram partow <pedram.p@bit24.info>
2026-02-09 01:28:50 +0330

    add-home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
-import { useTranslations } from 'next-intl';
-
-export default function Home() {
-  const t = useTranslations();
-
+export default function HomePage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <span className="body-01 text-zinc-600">description</span>
-      </main>
+    <div className="h-screen overflow-y-auto snap-y snap-mandatory">
+      <section className="h-screen snap-start snap-always flex items-center justify-center bg-gray-13">
+        <span className="headline-06 text-gray-1">Section 1</span>
+      </section>
+
+      <section className="h-[150vh] snap-start snap-always flex items-center justify-center bg-gray-12">
+        <span className="headline-06 text-gray-1">Section 2</span>
+      </section>
+
+      <section className="h-screen snap-start snap-always flex items-center justify-center bg-gray-11">
+        <span className="headline-06 text-gray-1">Section 3</span>
+      </section>
+
+      <section className="h-screen snap-start snap-always flex items-center justify-center bg-gray-12">
+        <span className="headline-06 text-gray-1">Section 4</span>
+      </section>
+
+      <section className="h-screen snap-start snap-always flex items-center justify-center bg-gray-13">
+        <span className="headline-06 text-gray-1">Section 5</span>
+      </section>
     </div>
   );
 }
