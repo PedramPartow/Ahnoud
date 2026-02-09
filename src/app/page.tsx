18f4e18b6d212208498b8b0f83ba89d13999d311
pedram partow <pedram.p@bit24.info>
2026-02-09 23:16:48 +0330

    add-bg-section-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import HomeHeaderSection from "@/components/home/HomeHeaderSection";
+
 export default function HomePage() {
   return (
     <div className="h-screen overflow-y-auto snap-y snap-mandatory">
-      <section className="h-screen snap-start snap-always flex items-center justify-center bg-gray-13">
-        <span className="headline-06 text-gray-1">Section 1</span>
+      <section 
+        className="relative h-screen snap-start snap-always flex items-center justify-center bg-cover bg-center bg-no-repeat px-5 md:px-20 py-6" 
+        style={{ backgroundImage: "url('/images/webp/section1.webp')" }}
+      >
+        <div className="absolute inset-0 bg-black/50" />
+        <div className="relative z-10">
+          <HomeHeaderSection />
+        </div>
       </section>
 
       <section className="h-[150vh] snap-start snap-always flex items-center justify-center bg-gray-12">
---
+++ src/components/home/HomeHeaderSection.tsx
@@ -0,0 +1,9 @@
+const HomeHeaderSection = () => {
+    return (
+        <div className="w-full">
+            <span className="text-gray-1">HomeHeaderSection</span>
+        </div>
+    );
+}
+
+export default HomeHeaderSection;
