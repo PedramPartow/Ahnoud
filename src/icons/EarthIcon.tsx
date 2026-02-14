88a4db145f5d207dea2580cf1a895d6b18fcd580
cavidyrm <javid.yrm@gmail.com>
2026-02-14 13:44:15 +0330

    added dockerfile

--- src/icons/EarthIcon.tsx
+++ src/icons/EarthIcon.tsx
@@ -10,7 +10,7 @@ export function MailIcon({
   }: IconProps) {
     return (
         <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <mask id="mask0_511_7514" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+        <mask id="mask0_511_7514" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
         <rect width="24" height="24" fill="#D9D9D9"/>
         </mask>
         <g mask="url(#mask0_511_7514)">
